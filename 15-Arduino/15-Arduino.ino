ee2e5872cfa825fdebb16a3b510bba3cf096105e
Daniele <Venturin>
2019-12-19 15:23:17 +0100

    Check vittoria + modifica random

--- 15-Arduino/15-Arduino.ino
+++ 15-Arduino/15-Arduino.ino
@@ -101,6 +101,22 @@ void loop() {
   }
 }
 
+void checkvittoria(){
+  bool check = false;
+  if(matrix[0][0] == 1 && matrix[0][1] == 2 && matrix[0][2] == 3 && matrix[0][3] == 4){
+    if(matrix[1][0] == 5 && matrix[1][1] == 6 && matrix[1][2] == 7 && matrix[1][3] == 8){
+      if(matrix[2][0] == 9 && matrix[2][1] == 10 && matrix[2][2] == 11 && matrix[2][3] == 12){
+        if(matrix[3][0] == 13 && matrix[3][1] == 14 && matrix[3][2] == 15 && matrix[3][3] == 0){
+          start = false;
+          lcd.clear();
+          resetMatrix();
+          TopStructure("Hai vinto!");
+        }
+      }
+    }
+  }
+}
+
 void grafica(){
   printlcd(14,0, "Mosse");
   printlcd(16,2, String(mosse));
@@ -125,6 +141,8 @@ void printMatrix(){
     }
     c = 0;
   }
+  //delay(200);
+  checkvittoria();
 }
 
 void resetMatrix(){
@@ -141,7 +159,7 @@ void genMatrix(){
   resetMatrix();
   int ponte;
   int rnd = random(1, 5);
-  for(int i = 0; i < 200; i++){
+  for(int i = 0; i < 15; i++){
     switch(rnd){
       case 1:
         up();
